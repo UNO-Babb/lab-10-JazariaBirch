2922ae1ae0050e3addec22d780fe661c33921d37
JazariaBirch <JazariaBirch@unomaha.edu>
2025-04-16 15:02:29 +0000

    done :|

--- main.py
+++ main.py
@@ -2,3 +2,24 @@
 #Name:
 #Date:
 #Assignment:
+
+
+import smoking
+report = smoking.get_report()
+
+num_items = len(report)
+total = []
+percent = []
+
+for spot in range(25):
+    smoke = report[0]["Data"]["Smokers"]["Total"]
+    smoking_total = report[0]["Data"]["Percentage"]["Total"]
+
+    
+    total.append(smoke)
+    percent.append(smoking_total)
+
+    import matplotlib.pyplot as plt
+    plt.plot(total, percent, 'ro')
+    plt.ylabel('Total Score')
+    plt.savefig("output")
